0b790c10a3a3f9f08fb020d88424c4f0b0364fb7
zamcham <cristian@zamcham.com>
2023-03-11 15:17:01 -0500

    Update linters discrepancies

--- Scripts/mainscripts.js
+++ Scripts/mainscripts.js
@@ -1,10 +1,11 @@
+const pixels = 'px';
 const heroSection = document.querySelector('.hero-section');
 
 function updateHeroHeight() {
   const heroHeight = heroSection.clientHeight + window.innerHeight * 0.05;
   const heroHeightDesktop = heroSection.clientHeight + window.innerHeight * 0.2;
-  heroSection.style.setProperty('--before-height', heroHeight + 'px');
-  heroSection.style.setProperty('--before-height-desktop', heroHeightDesktop + 'px');
+  heroSection.style.setProperty('--before-height', heroHeight + `${pixels}`);
+  heroSection.style.setProperty('--before-height-desktop', heroHeightDesktop + `${pixels}`);
 }
 
 window.addEventListener('resize', updateHeroHeight);
