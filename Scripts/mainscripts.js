078163587c73c9e532dbcd106d03cb0247464bc8
zamcham <cristian@zamcham.com>
2023-03-15 15:05:32 -0400

    Update to fix errors

--- Scripts/mainscripts.js
+++ Scripts/mainscripts.js
@@ -142,8 +142,7 @@ function init() { // eslint-disable-line no-unused-vars
   const closeButtonClass = document.querySelector('.x_button');
   const body = document.querySelector('body');
   const computedStyle = getComputedStyle(body);
-  <!-- eslint-disable-next-line max-len -->
-  const { overflow } = computedStyle; function showDropDownMenu() { // eslint-disable-line no-unused-vars
+  const { overflow } = computedStyle; function sDM() { // eslint-disable-line no-unused-vars
     dropDownMenu.classList.toggle('open');
     openButtonClass.classList.toggle('hidden');
     closeButtonClass.classList.toggle('hidden');
@@ -153,7 +152,7 @@ function init() { // eslint-disable-line no-unused-vars
       document.body.style.overflow = 'hidden';
     }
   }
-  toggleButton.onclick = showDropDownMenu;
-  closeButton.onclick = showDropDownMenu;
+  toggleButton.onclick = sDM;
+  closeButton.onclick = sDM;
 }
 // #endregion
