a5088f241aad51300fab2749a41dfec1629e15ae
zamcham <cristian@zamcham.com>
2023-03-22 15:59:45 -0400

    Fix ESlint errors

--- Scripts/mainscripts.js
+++ Scripts/mainscripts.js
@@ -161,11 +161,11 @@ const background1 = document.getElementById('bg1');
 const background2 = document.getElementById('bg2');
 const background3 = document.getElementById('bg3');
 const background4 = document.getElementById('bg4');
-window.addEventListener('scroll', function() {
-  let value = window.scrollY;
-  background1.style.top = value * 0.6 + 'px';
-  background2.style.top = value * 0.8 + 'px';
-  background3.style.top = value * 0.4 + 'px';
-  background4.style.top = value * 0.9 + 'px';
+window.addEventListener('scroll', () => {
+  const value = window.scrollY;
+  background1.style.top = `${value * 0.6}px`;
+  background2.style.top = `${value * 0.8}px`;
+  background3.style.top = `${value * 0.4}px`;
+  background4.style.top = `${value * 0.9}px`;
 });
 // #region
